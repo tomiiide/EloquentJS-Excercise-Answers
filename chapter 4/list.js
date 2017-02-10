9a00bf1f251b40357c3330ba38ff3e008883a94d
tomiiide <ayotomiiide@gmail.com>
2017-02-10 21:21:52 +0100

    Chapter 4 - Lists completed

--- chapter 4/list.js
+++ chapter 4/list.js
@@ -1,15 +1,46 @@
-var list = {
-  value: 1,
-  rest: {
-    value: 2,
-    rest: {
-      value: 3,
-      rest: null
-    }
-  }
-};
 
 function arrayToList(array)
 {
-
+  var lists = {};
+  for (var index = array.length-1; index >= 0; index--) {
+    if (lists.value)
+      lists = {
+        value: array[index],
+        rest: lists
+      };
+    else 
+      lists = {
+        value: array[index],
+        rest: null
+      };
+  }
+  return lists;
 }
+
+function prepend(value, list)
+{
+  list = {
+    value: value,
+    rest: list
+  };
+  return list;
+}
+
+function nth(n,list)
+{
+  if (n == 1)
+    return list.value;
+  if (list.rest == null)
+    return undefined;
+  if (n == 0)
+    return null;
+  return nth(n - 1, list.rest);
+}
+
+
+//test
+var numbers = [3, 5, 6, 4];
+// console.log(arrayToList(numbers));
+newList = prepend(5, arrayToList(numbers));
+// console.log(newList);
+console.log(nth(2, newList));
